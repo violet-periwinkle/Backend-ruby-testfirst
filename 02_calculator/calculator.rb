88a109fff0d659aa570ea5151a3804d303ffb5cb
violet-periwinkle <134008799+violet-periwinkle@users.noreply.github.com>
2024-07-21 10:08:27 -0400

    lesson 5 commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,12 @@
 #write your code here
+def add(a, b)
+    a + b
+end
+def subtract(a, b)
+    a - b
+end
+def sum(array)
+    result = 0
+    array.each { |addend| result += addend }
+    result
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+    @title
+    def title
+        @title
+    end
+    def title=(title)
+        words = title.split(" ")
+        little_words = ["a", "an", "and", "as", "at", "but", "by", "down", "for", "from", "if", "in", "into", "like", "near", "nor", "of", "off", "on", "once", "onto", "or", "over", "past", "so", "than", "that", "the", "to", "upon", "with"]
+        words.map do |str|
+            unless little_words.include?(str.downcase)
+            str.capitalize!
+            end
+        end
+        words[0].capitalize!
+        @title = words.join(" ")
+    end
 end
